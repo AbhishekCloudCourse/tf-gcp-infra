1793b2c61ceee4c5c6d3aac37d3ad804afa7cd31
Abhishek Unnithan <gopalakrishnanunni.a@northeastern.edu>
2024-02-21 12:48:10 -0500

    Testbranch (#6)
    * first commit
    
    * update readme
    
    * changes
    
    * worklow changes
    
    * updated workflow
    
    * vars
    
    * validate terraform
    
    * terraform update
    
    * update

--- infra/main.tf
+++ infra/main.tf
@@ -1,28 +1,34 @@
 # Create VPC
-resource "google_compute_network" "gcp_vpc" {
-  name                    = "my-vpc"
-  routing_mode            = "REGIONAL"
+resource "google_compute_network" "vpc" {
+  count = length(var.gcp_vpc)
+  name = var.gcp_vpc[count.index].name
+  routing_mode = var.gcp_vpc[count.index].routing_mode
   delete_default_routes_on_create = true
   auto_create_subnetworks = false
 }
 
 # Create Subnets
 resource "google_compute_subnetwork" "webapp_subnet" {
-  name          = "webapp"
-  ip_cidr_range = "10.0.1.0/24"
-  network       = google_compute_network.gcp_vpc.id
+    count = length(var.gcp_vpc)
+    name = var.gcp_vpc[count.index].subnet_name_1
+    ip_cidr_range = var.gcp_vpc[count.index].subnet_1_cidr
+    network = google_compute_network.vpc[count.index].id 
+    region = var.gcp_region
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
-  name          = "db"
-  ip_cidr_range = "10.0.2.0/24"
-  network       = google_compute_network.gcp_vpc.id
+   count = length(var.gcp_vpc)
+   name = var.gcp_vpc[count.index].subnet_name_2
+   ip_cidr_range = var.gcp_vpc[count.index].subnet_2_cidr
+   network = google_compute_network.vpc[count.index].id 
+   region = var.gcp_region
 }
 
-# Create Route for Internet Gateway
+# Create Route for Internet Gatewa
 resource "google_compute_route" "internet_route" {
-  name                  = "internet-route"
-  network               = google_compute_network.gcp_vpc.name
-  dest_range            = "0.0.0.0/0"
-  next_hop_gateway      = "default-internet-gateway"
+    count = length(var.gcp_vpc)
+    name = var.gcp_vpc[count.index].subnet_1_custom_route
+    network = google_compute_network.vpc[count.index].id
+    dest_range = "0.0.0.0/0"
+    next_hop_gateway = "default-internet-gateway"
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -10,3 +10,17 @@ variable "gcp_project"{
 variable "gcp_region"{
     
 }
+
+variable "gcp_vpc" {
+  type = list(
+    object({
+      name                  = string
+      routing_mode          = string
+      subnet_name_1         = string
+      subnet_1_cidr         = string
+      subnet_name_2         = string
+      subnet_2_cidr         = string
+      subnet_1_custom_route = string
+    })
+  )
+}
